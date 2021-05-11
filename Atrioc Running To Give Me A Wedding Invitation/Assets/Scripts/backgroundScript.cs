ce85f79976d1979cc330bf177f19655943f90222
Gavin Hawk <56935803+Gavin-Hawk@users.noreply.github.com>
2021-05-10 21:50:57 -0500

    Base Game

---
+++ Atrioc Running To Give Me A Wedding Invitation/Assets/Scripts/backgroundScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundScript : MonoBehaviour
+{
+    public Vector3 start;
+    public float speed;
+    private int time;
+    public int timeCap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        start = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time++;
+        transform.position = transform.position + new Vector3( speed, 0, 0);
+
+        if (time >= timeCap)
+        {
+            transform.position = start;
+            time = 0;
+        }
+    }
+}
---
+++ Atrioc Running To Give Me A Wedding Invitation/Assets/Scripts/platformer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformer : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    public float jumpForce;
+    bool isGrounded = false;
+    public Transform isGroundedChecker;
+    public float checkGroundRadius;
+    public LayerMask groundLayer;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public float rememberGroundedFor;
+    float lastTimeGrounded;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Jump();
+        BetterJump();
+        Move();
+        CheckIfGrounded();
+    }
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float moveBy = x * speed;
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    void BetterJump()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void CheckIfGrounded()
+    {
+        Collider2D colliders = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
+        if (colliders != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            if (isGrounded)
+            {
+                lastTimeGrounded = Time.time;
+            }
+            isGrounded = false;
+        }
+    }
+}
